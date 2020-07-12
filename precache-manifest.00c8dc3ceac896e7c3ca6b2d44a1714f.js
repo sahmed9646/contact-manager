4ffd49aeeed62095593d5a2670c007cb45719d0a
sahmed9646 <sabbir9646@gmail.com>
2020-07-12 11:23:06 +0100

    Updates

---
+++ precache-manifest.00c8dc3ceac896e7c3ca6b2d44a1714f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c73637b3071951d4917b18baf72dec",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "b437ebe635da3aa4a6f5",
+    "url": "/contact-manager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "265d8012d68a9c35f433",
+    "url": "/contact-manager/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "b437ebe635da3aa4a6f5",
+    "url": "/contact-manager/static/js/2.e2278db9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contact-manager/static/js/2.e2278db9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "265d8012d68a9c35f433",
+    "url": "/contact-manager/static/js/main.b70710bb.chunk.js"
+  },
+  {
+    "revision": "ad86e1d3d1dedce9579f",
+    "url": "/contact-manager/static/js/runtime-main.e019823c.js"
+  }
+]);
